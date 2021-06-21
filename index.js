3a679cf99843fb28f20f27db182067c7ae1e61b0
Rene <renegarcia1864@gmail.com>
2021-06-21 17:45:04 -0500

    commit tree f589610f56e24d68abc592783fc5fca20ce83d4c

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) {
+     return 'This one is on me!';
+  } else if (1999 < feet && feet < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination){
   // Write your code here!
+  if (destination === 'NYC')
+    return "Ok, sounds good."
+  else 
+    return 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+    if (tip === 'generous') {
+      return "Thank you so much.";
+    } else if (tip === 'not as generous') {
+      return 'Thank you.';
+    } else {
+      return "Bye." ;
+    }
+};
